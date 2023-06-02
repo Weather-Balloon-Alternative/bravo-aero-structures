60699917773b2c399fd7c01f7be4668f0ca85b7c
dolkslab <dolkslab@gmail.com>
2023-06-02 17:28:44 +0200

    begin work on section property analysis

--- structures/struct_basic_calc.py
+++ structures/struct_basic_calc.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import scipy.integrate
 
 
-def I_circ(d):
-	return (1/64)*np.pi*d**4
+
+
 
 def get_aero_loads(y, W, b, c, n, Ar, e, CLmax, Cmac):
 	#Calculate the loads at a location on the wing based on a given weight and load factor
@@ -18,48 +18,56 @@ def get_aero_loads(y, W, b, c, n, Ar, e, CLmax, Cmac):
 
 
 def gen_airfoil_shape(c, tc, plot=False):
-	#generate an airfoil profile based on a scaled naca 2412
+	#generate an airfoil profile based on a scaled naca 2412, just used as a generic shape for now
 	points = np.genfromtxt('NACA2412.dat', delimiter=',').T
 	n_half = 41
 	
-	points[0] *= c
-	points[1] *= (tc/0.12)
+	points[0] *= c #scale the points by dimensions given
+	points[1] *= (tc/0.12)*c # the 0.12 is the default t/c ratio of the airfoil used
 
-	area = -scipy.integrate.simpson(points[1,:n_half], points[0,:n_half]) - scipy.integrate.simpson(points[1,n_half:], points[0,n_half:]) #idk why the first integral is negative
-	print(points[0,:n_half])
+	area = -scipy.integrate.simpson(points[1,:n_half], points[0,:n_half]) - scipy.integrate.simpson(points[1,n_half:], points[0,n_half:])
+	#print(points[0,:n_half])
 	print(area)
 
 	if plot:
 		plt.plot(points[0], points[1])
+		plt.grid(visible=True)
 		plt.show()
-	#upper points, lower points, cross-sectional area
-	return points[:,:n_half], points[:,n_half:], area
+	#all points, upper points, lower points, cross-sectional area
+	return points, points[:,:n_half], points[:,n_half:], area
 
 def wing_volume(cr, tapre_rat, span, tc=0.12):
-	A_root = gen_airfoil_shape(cr, tc)[2]
-	A_tip = gen_airfoil_shape(cr*tapre_rat, tc)[2]
+	A_root = gen_airfoil_shape(cr, tc)[3]
+	A_tip = gen_airfoil_shape(cr*tapre_rat, tc)[3]
 	V = span*(A_root+A_tip)*0.5
 	return V
 
-	
 
-def spar_sizing(W, b, n, d_max, mat_prop):
-	#calculate moment assuming uniform lift distribution
-	M_root = W*n*0.5*0.25*b
-	I_req = (M_root*d_max*0.5)/mat_prop[0]
-	d_inner = ((I_circ(d_max)-I_req)*(64/np.pi))**0.25
-	return(d_inner, (d_max-d_inner)*0.5)
+def planform(b, Ar, tapre_rat):
+	#calculate wing platform properties based on span, aspect ratio and tapre ratio
+	S = (b**2)/Ar
+	mac = b/Ar
+	c_root = (2*S)/((1+tapre_rat)*b)
+	c_tip = tapre_rat*c_root
+	return {"S":S, "mac":mac, "c_root":c_root, "c_tip":c_tip, "span":b, "aspect":Ar, "tapre":tapre_rat}
+
+
 
 
 	
 if __name__=="__main__":
 	Wt = 25
 	bt = 1.5
+	ARt = 8
+	tapre_ratt = 0.6
+	planform_prop = planform(bt, ARt, tapre_ratt)
+	print(planform_prop)
+
 	nt = 2.5
 	d_max_t = 7e-3
 
 	mat_prop_cfrp = [650e6]
 	res = spar_sizing(Wt, bt, nt, d_max_t, mat_prop_cfrp)
 	print(res[0]*1000, res[1]*1000)
-	gen_airfoil_shape(1, 0.16)
-	print(wing_volume(0.3, 0.6, bt)*25*1000)
+	gen_airfoil_shape(1, 0.16, plot=True)
+	print(wing_volume(planform_prop['c_root'], tapre_ratt, bt)*25*1000)
