7ec4e398ca00b21d93c9bc537cd34f5f4033b577
MennoWijkhuizen <95078439+MennoWijkhuizen@users.noreply.github.com>
2023-06-05 21:27:11 +0200

    solved excel issue, commented aeroloader, started trying to identify which coeeficients belong where in coefficients.py

--- aeroloader.py
+++ aeroloader.py
@@ -2,22 +2,44 @@ import numpy as np
 import pandas as pd
 
 def loadaero(filepath):
+    """
+    ARGS:
+            filepath: string,           name or route to a specific .xlsx or .xlsm file.
+    OUTPUTS:
+            output: dictionary,         dictionary with all coefficients
+    """
+
+    #read the file to numpy
     df = pd.read_excel(filepath)
     data = df.to_numpy()
+    
+    #get a list of all names that are mentioned in the excel file
     namesraw = data[:,0]
     names = np.array(namesraw)
+    
+    #ensure that all elements in the dictionary will get a unique name
     for name in names:
         if len(np.where(names == name)[0]) > 1:
+
+            #find the locations of the copies
             copy = np.where(names == name)[0]
+
+            #change names to specific names
             for i in range(1,len(np.where(namesraw == name)[0])):
                 names[copy[i]] = namesraw[copy[i]] + f'{i}'
 
+    #create output dictionary
     output = {}
+
+    #initialize creation of elements in ouput by making range with indicies
     for i in range(len(names)):
         # output[names[i]] = list(filter(lambda x: str(x) != 'nan', data[i,:]))
+
+        # create for every element a list with all column data in case the data itself is not equal to not a number.
         output[names[i]] = [obj for obj in data[i,:] if str(obj) != 'nan']
     return output
 
 
-k = loadaero('Aero_output/glider_v7.xlsx')
-print(k)
+if __name__ =="__main__":
+    k = loadaero('Aero_output/glider_v7.xlsx')
+    print(k)
