a0adb3db91756caaf261185a4cdfa4ff86bc5a21
mcotte <mcotte@student.tudelft.nl>
2023-06-08 14:42:53 +0200

    Update Tail_boom_sizing.py

--- structures/Tail_boom_sizing.py
+++ structures/Tail_boom_sizing.py
@@ -18,11 +18,10 @@ def shear(n,W,b,S_h,S):
 def tube_stresses(M_x, V_y, T_z):
     # Initialize radius and thickness options
     r_range = np.linspace(r_min, r_max, int(r_max/r_step + 1))
-    design_options = [0., 0., 0., 0., 0.,0.,0.]
+    design_options = [0., 0., 0., 0., 0.,0.,0.,0.]
 
     for id in range(len(r_range)):
         r = r_range[id]
-
         r_array = np.ones((id+1))*r
         t_range = np.linspace(t_min, r, (id+1))
 
@@ -30,6 +29,7 @@ def tube_stresses(M_x, V_y, T_z):
         I_xx = (np.pi * r_array**4) / 4 - (np.pi * (r_array - t_range)**4) / 4
         J = (np.pi * r_array**4) / 2 - (np.pi * (r_array - t_range)**4) / 2
         A = np.pi * (r_array**2 - (r_array-t_range)**2)
+        mass_per_metre = A * CFRP_density
 
         # stresses for solid tube
         bending_stress = M_x * r_array / I_xx
@@ -42,7 +42,7 @@ def tube_stresses(M_x, V_y, T_z):
         vert_def = M_x * l_tail**2 / (3*E*I_xx)
 
         for idx in range(len(t_range)):
-            section_properties = [r, t_range[idx], A[idx], twist[idx], vert_def[idx],bending_stress[idx],shear_stress[idx]]
+            section_properties = [r, t_range[idx], A[idx], twist[idx], vert_def[idx],bending_stress[idx],shear_stress[idx],mass_per_metre[idx]]
             design_options = np.vstack((design_options,section_properties))
 
 
@@ -52,8 +52,8 @@ def tube_stresses(M_x, V_y, T_z):
 
 if __name__ == "__main__":
     # load case
-    n = 4.5
-    W = 0.92504571*9.81
+    n = 2.5
+    W = 0.79948938*9.81
     # maximum deformatations and stresses
     safety_factor  = 1.4
     max_twist = 2 * np.pi/180 / safety_factor
@@ -61,15 +61,15 @@ if __name__ == "__main__":
     CFRP_yield_stress = 110 * 10**9 / safety_factor
     CFRP_shear_strength = 260 * 10**9 / safety_factor
     # aircraft geometry
-    S = 0.09
-    S_h = 0.011833342
-    b = 1.161895004/2
-    b_h =  0.21756233/2
-    l_tail = 0.407380764
+    S = 0.05
+    S_h = 0.0066762
+    b = 0.774596669/2
+    b_h =  0.141522441/2
+    l_tail = 0.413262035
     # numerical parameters
     r_min = 0.001
     r_max = 0.01
-    r_step = 0.001
+    r_step = 0.0005
     t_min = 0.0005
     t_max = 0.01
     t_step = 0.0005
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     V_y, T_z = shear(n, W, b, S_h, S)
 
     # deformations
-    r,t,A,twist,vert_def,bending_stress,shear_stress = 0,1,2,3,4,5,6
+    r,t,A,twist,vert_def,bending_stress,shear_stress, = 0,1,2,3,4,5,6
     design_options = tube_stresses(M_x,V_y,T_z)
 
     A_array = design_options[:,A]
@@ -93,15 +93,21 @@ if __name__ == "__main__":
     bending_stress_array = design_options[:,bending_stress]
     shear_stress_array = design_options[:,shear_stress]
 
-    print(design_options[38])
-    mass_per_metre = design_options[38,A]*CFRP_density
+    viable_options = np.where((twist_array <= max_twist) & (vert_def_array <= max_vert_def) & (bending_stress_array <= CFRP_yield_stress) & (shear_stress_array <= CFRP_shear_strength))
+    MVoption_id = np.min(viable_options)
+    viableA = A_array[52:]
+    optimal_option_id = np.argmin(viableA)
+    optimal_option = design_options[optimal_option_id]
+    print(len(viable_options[0]))
+
+
+
+    print(optimal_option)
+
+    mass_per_metre = optimal_option[A] * CFRP_density
     print(mass_per_metre)
-    print('')
-    print(design_options)
-    # plt.plot(A_array,twist_array,color='green',label='twist')
-    # plt.plot(A_array,vert_def_array,color='red',label='vertical deformation')
-    # plt.show()
-    #
-    # plt.plot(A_array,bending_stress_array, color='green', label='bending_stress')
-    # plt.plot(A_array, shear_stress_array, color='red', label='torsional_shear')
-    # plt.show()
+
+    plt.plot(range(174),A_array[(231-174):])
+    plt.plot(range(174),design_options[(231-174):,0])
+    plt.plot(range(174),design_options[(231-174):,1])
+    plt.show()
